dc409a3dc1f9acd273e4074bda1f278789d7a93f
Typhaine Le Gales <tiphaine.legales@neuf.fr>
2019-05-15 00:18:52 +0200

    test onclick onmouseover onmouseout map area

--- display.js
+++ display.js
@@ -6,3 +6,17 @@ function closeDescription () {
    document.getElementById('descriptionProjet').style.display="none";
   }
 });
+
+function detailFarmer () {
+  document.getElementById('wrapper').style.backgroundColor = "red";
+}
+
+function pictureFarmerOn () {
+  document.getElementById('picture').style.display = "block";
+  console.log('on');
+}
+
+function pictureFarmerOff () {
+  document.getElementById('picture').style.display = "none";
+  console.log('off');
+}
