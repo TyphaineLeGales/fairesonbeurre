0d75639e4a1cc9c3864977d3d3d08d14070290eb
Typhaine Le Gales <tiphaine.legales@neuf.fr>
2019-06-10 19:09:17 +0200

    layout 2eme

--- display.js
+++ display.js
@@ -1,18 +1,23 @@
 
 document.addEventListener('DOMContentLoaded', function() {
-  document.getElementById('close').addEventListener("click", closeDescription, false);
+  document.getElementById('close').addEventListener("click", closeDescription);
   toggle();
-  closeDescription();
 });
 
 function toggle () {
   if(document.getElementById("toggleButton").checked == true) {
-    document.getElementById('wrapper').style.backgroundColor = "#F9E998";
+    document.getElementById('wrapper').style.backgroundColor = "#E5E9DD";
+    document.getElementById('legende').style.display = "block";
+    document.getElementById('video').style.display = "none";
   } else {
     document.getElementById('wrapper').style.backgroundColor = "#E5E9DD";
+    document.getElementById('legende').style.display = "none";
+    document.getElementById('video').style.display = "block";
   }
 }
 
 function closeDescription () {
    document.getElementById('descriptionProjet').style.display="none";
   }
+
+
